b0c244a0ca1df71bb1ea1c2b43a48f7eafbae648
origami unicorn <rvwright@gmail.com>
2020-01-18 00:55:00 -0800

    Determining a comparison between two arrays and absolute values of their difference per each index.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,30 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        res.json(req.body);
-        console.log(req.body);
+        // console.log(req.body);
+        // console.log(req.body.data.scores);
+
+        let newFriend = req.body.data.scores;
+        console.log(newFriend);
+        console.log(friendsData[0].scores);
+
+        compareArr(newFriend, friendsData[0].scores);
+
+        // for (let i = 0; i < friendsData.length; i++) {
+
+        // }
+
+        // want to push to array but before/after checking all values?
     });
 
 };
+
+function compareArr(arr1, arr2) {
+    const sumDiff = arr1.map(function (num, idx) {
+        return Math.abs(parseInt(num) - arr2[idx]);
+    });
+
+    console.log("Absolute Values between two arrays", sumDiff);
+}
+
+
