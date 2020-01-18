a24158162a7476a75101e0d71f7dfec603e75034
origami unicorn <rvwright@gmail.com>
2020-01-18 05:13:42 -0800

    Added in the questions, added in data for friends.js for ten different individuals.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,162 @@
 let friends = [
     {
-        name: "Bill Murray",
-        photo: "/images/borutosdad.png",
-        scores: [
-            2,
-            3,
-            5
+        "name": "Boruto's Dad",
+        "photo": "/images/borutosdad.png",
+        "scores": [
+            "3",
+            "2",
+            "5",
+            "4",
+            "5",
+            "5",
+            "1",
+            "2",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Dwayne 'The Rock' Johnson",
+        "photo": "/images/dwaynejohnson.png",
+        "scores": [
+            "5",
+            "3",
+            "5",
+            "2",
+            "2",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Ellen Degeneres",
+        "photo": "/images/ellendegeneres.png",
+        "scores": [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "5",
+            "3",
+            "4",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Ellen Ripley",
+        "photo": "/images/ellenripley.png",
+        "scores": [
+            "5",
+            "5",
+            "1",
+            "3",
+            "3",
+            "5",
+            "5",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Leia Organa Solo",
+        "photo": "/images/leiaorganasolo.png",
+        "scores": [
+            "4",
+            "5",
+            "3",
+            "1",
+            "3",
+            "1",
+            "5",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Lucy Liu",
+        "photo": "/images/lucyliu.png",
+        "scores": [
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "3",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Rose DeWitt Bukater",
+        "photo": "/images/rosedewittbukater.png",
+        "scores": [
+            "1",
+            "1",
+            "4",
+            "3",
+            "1",
+            "4",
+            "1",
+            "4",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Sephiroth",
+        "photo": "/images/sephiroth.png",
+        "scores": [
+            "1",
+            "3",
+            "1",
+            "5",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Wei Wuxian",
+        "photo": "/images/weiwuxian.png",
+        "scores": [
+            "5",
+            "5",
+            "3",
+            "4",
+            "5",
+            "5",
+            "1",
+            "5",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Yoshi",
+        "photo": "/images/yoshi.png",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "1",
+            "5",
+            "1",
+            "1"
         ]
     }
 ];
